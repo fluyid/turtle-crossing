ba3be2d35378249bf6a762c44a64ad71ad6a0fa9
fluyid <kailashnathashok@gmail.com>
2024-04-15 16:43:14 +0300

    making the scoreboard and game over part

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
-# from scoreboard import Scoreboard
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,9 +10,11 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkeypress(player.go_up, "Up")
+screen.onkeypress(player.go_down, "Down")
 
 game_is_on = True
 while game_is_on:
@@ -25,9 +27,11 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
 
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        scoreboard.increase_level()
 
 screen.exitonclick()
